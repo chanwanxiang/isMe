6a01452412f3802e5914a33e0056e774a0a7449e
wxchan <595366700@qq.com>
2021-02-25 20:38:41 +0800

    keep coding

--- Read-Search-Ask/【1】Python/黑马2020/2-Python和Linux高级编程/2-web服务器V5.0/1-网络编程基础/5-TCP客户端程序开发.py
+++ Read-Search-Ask/【1】Python/黑马2020/2-Python和Linux高级编程/2-web服务器V5.0/1-网络编程基础/5-TCP客户端程序开发.py
@@ -2,7 +2,12 @@
 
 # 1. TCP客户端程序开发示例代码
 
+# 导入socket模块
 import socket
 
 if __name__ == '__main__':
+    # 创建TCP客户端套接字
+    # 1.AF_INET:表示ipv4
+    # 2. SOCK_STEAM:tcp传输协议
+    ClientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
