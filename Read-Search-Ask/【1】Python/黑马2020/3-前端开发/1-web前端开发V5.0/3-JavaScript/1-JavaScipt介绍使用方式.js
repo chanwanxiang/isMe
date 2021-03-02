6758584fe8921195b6dedb1f2413cf23c3c80e0c
wxchan <595366700@qq.com>
2021-03-02 15:27:17 +0800

    keep coding

---
+++ Read-Search-Ask/【1】Python/黑马2020/3-前端开发/1-web前端开发V5.0/3-JavaScript/1-JavaScipt介绍使用方式.js
@@ -0,0 +1,32 @@
+/*
+JavaScript介绍
+
+1. JavaScript定义
+    JavaScript是运行在浏览器端的脚本语言,是由浏览器解释执行的,简称js,它能够让网页和用户有交互功能,增加料号的用户体验效果
+    前端开发三大模块
+        HTML 负责网页结构
+        CSS 负责网页样式
+        JavaScript  负责网页行为
+
+2. 小结
+    JavaScript是运行在浏览器端的脚本语言,它的作用就是负责网页和用户的交互效果
+
+JavaScript使用方式
+
+1. 行内式(主要用于事件)
+<input type="button" name="" onclick="alert('OK!')">
+
+2. 内嵌式
+<sript type="text/javascipt">
+    alert('OK!')
+</script>
+
+3. 外链式
+<script type="text/javascript" src="js/demo.js"></script>
+
+4. 小结
+    JavaScript的使用方式有三种,分别是
+        行内式
+        内嵌式
+        外链式
+*/
