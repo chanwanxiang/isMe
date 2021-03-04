badf9b47dbcb6a44af9ddab42ca57b75eb16ba37
wxchan <wxchannive@gmail.com>
2021-03-05 00:16:02 +0800

    keep coding

---
+++ Read-Search-Ask/Python/黑马2020/4-web开发/2-Demo/mains/addNum/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from models import cal
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def calPage(request):
+    return render(request,'cal.html')
+
+def calc(request):
+    valA = request.POST['valueA']
+    valB = request.POST['valueB']
+    result = int(valA) + int(valB)
+
+    cal.objects.create(valueA=valueA,valueB=valueB,result=result)
+
+    return render(request,'rlt.html',context={'data':{result}})
