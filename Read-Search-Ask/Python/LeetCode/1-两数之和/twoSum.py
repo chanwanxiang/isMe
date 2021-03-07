d06d818763566a432903fa840df157fa098f0d80
wxchan <wxchannive@gmail.com>
2021-03-08 00:40:24 +0800

    keep coding

--- Read-Search-Ask/Python/LeetCode/1-两数之和/twoSum.py
+++ Read-Search-Ask/Python/LeetCode/1-两数之和/twoSum.py
@@ -8,27 +8,51 @@
 # 链接：https://leetcode-cn.com/problems/two-sum
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-class Solution(object):
-    def twoSum(self,nums,target):
-        """
-        description : 两数之和
-        param : nums-list,target-int
-        returns : list
-        """
+# 方式一
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         dt = {}
+#         for i, num in enumerate(nums):
+#             if target - num in dt:
+#                 return [dt[target-num], i]
+#             # 记录数字下标
+#             dt[num] = i
 
-        dt = {}
 
-        for i,num in enumerate(nums):
-            if target - num in dt:
-                return [dt[target-num],i]
-            # 记录数字下标
-            dt[num] = i
-        
+# s = Solution()
+# print(s.twoSum([1, 2, 3], 3))  # [0, 1]
+
+# 方式二:
+# class Solution:
+#     def twoSum(self,nums,target):
+#         for i in nums:
+#             j = target - i
+#             startindex = nums.index(i)
+#             nextindex = startindex + 1
+#             tempNums = nums[nextindex:]
+#             if j in tempNums:
+#                 return(nums.index(i),nextindex+tempNums.index(j))
+
+# s = Solution()
+# print(s.twoSum([1,2,3],4))
+
+# 方式三
+class Solution:
+    def twoSum(self, nums, target):
+        dc = dict()
+        for i in range(len(nums)):
+            if target - nums[i] not in dc:
+                dc[nums[i]] = i
+            else:
+                return [dc[target-nums[i]], i]
+
+
 s = Solution()
-print(s.twoSum([1,2,3],3))  #[0, 1]
-        
+print(s.twoSum([1, 2, 3], 4))
+
+
 # 扩展
-    # enumerate(sequence, [start=0])
-    # sequence -- 一个序列、迭代器或其他支持迭代对象。
-    # start -- 下标起始位置。
-    # 返回 enumerate(枚举) 对象。
+# enumerate(sequence, [start=0])
+# sequence -- 一个序列、迭代器或其他支持迭代对象。
+# start -- 下标起始位置。
+# 返回 enumerate(枚举) 对象。
