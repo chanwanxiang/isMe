9270a2112b66ef9b66048c4a4b3d3c02b23b6e00
wxchan <595366700@qq.com>
2021-03-08 11:26:40 +0800

    keep coding

--- Read-Search-Ask/Python/LeetCode/2-两数相加/addTwoNum.py
+++ Read-Search-Ask/Python/LeetCode/2-两数相加/addTwoNum.py
@@ -1,8 +1,10 @@
-class Solution:
-    def addTwoNumbers(self,l1,l2):
-        if l1 = None:
-            return l2
-        if l2 = None:
-            return l1
+# class Solution:
+#     def addTwoNumbers(self,l1,l2):
+#         if l1 = None:
+#             return l2
+#         if l2 = None:
+#             return l1
 
-        dummy = listNode
+#         dummy = listNode
+
+print(2**31)
