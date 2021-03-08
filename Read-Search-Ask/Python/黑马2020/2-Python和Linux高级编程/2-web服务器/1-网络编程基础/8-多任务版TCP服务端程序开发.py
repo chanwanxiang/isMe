8628491bd73cd1a2d3baf9d55ad6bb6da40c6d02
wxchan <595366700@qq.com>
2021-03-08 16:36:08 +0800

    keep coding

--- Read-Search-Ask/Python/黑马2020/2-Python和Linux高级编程/2-web服务器/1-网络编程基础/8-多任务版TCP服务端程序开发.py
+++ Read-Search-Ask/Python/黑马2020/2-Python和Linux高级编程/2-web服务器/1-网络编程基础/8-多任务版TCP服务端程序开发.py
@@ -1,13 +1,14 @@
-## 多任务版TCP服务端程序开发
+# 多任务版TCP服务端程序开发
 
 # 1. TCP服务端程序开发示例代码
 
 # 导入socket模块
-import socket,threading
+import socket
+import threading
 
 
 # 处理客户端请求数据
-def handleClientRequest(newClient,ipPort):
+def handleClientRequest(newClient, ipPort):
     # 5. 循环接受客户端的数据
     while True:
         recvdata = newClient.recv(1024)
@@ -27,34 +28,38 @@ def handleClientRequest(newClient,ipPort):
     # 关闭和客户端通信
     newClient.close()
 
+
 if __name__ == '__main__':
     # 1. 创建TCP服务端套接字,serverSocket只复制等待接收客户端的连接请求,收发消息不使用该套接字
-    serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # socket.AF_INET 与IPV4地址族进行通信
+    # socket.SOCK_STREAM 面向连接socket,TCP协议
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 设置端口号复用,表示服务端程序退出端口号立即释放
     # SOL_SOCKET 表示当前套接字
     # SOL_REUSEADDR 表示复用端口号选项
-    serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,True)
-    
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
     # 2. 绑定端口号
     # 第一个参数表示IP地址,一般不用指定,表示本机的任何一个IP均可
     # 第二个参数表示端口号
-    serverSocket.bind(('',9090))
-    
+    serverSocket.bind(('', 9090))
+
     # 3. 设置监听
     # 128:表示最大等待建立连接的个数
     serverSocket.listen(128)
-    
+
     # 4. 等待接受客户端的连接请求
     # TODO: 当客户端和服务端建立连接成功,会返回一个新的套接字
-    
+
     # 循环等待接受客户端的连接请求
     while True:
         # 主线程专门等待接受客户端连接请求
-        newClient,ipPort = serverSocket.accept()
+        newClient, ipPort = serverSocket.accept()
         print(f'客户端IP和端口号为{ipPort}')
 
         # 当客户端与服务端建立连接成功,创建子线程,让子线程专门负责接受客户端的消息
-        subThread  = threading.Thread(target = handleClientRequest,args=(newClient,ipPort))
+        subThread = threading.Thread(
+            target=handleClientRequest, args=(newClient, ipPort))
         # 设置守护主线程,主线程退出子线程销毁
         subThread.setDaemon(True)
         # 启动子线程
