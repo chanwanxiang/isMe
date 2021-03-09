b8da1abd538837b235ac3cb70abb19d798784444
wxchan <595366700@qq.com>
2021-03-09 14:09:31 +0800

    keep coding

--- Read-Search-Ask/Python/爬虫/爬取表情/imageSpider.py
+++ Read-Search-Ask/Python/爬虫/爬取表情/imageSpider.py
@@ -1,4 +1,5 @@
-import os,requests
+import os
+import requests
 # 网页选择器
 from bs4 import BeautifulSoup
 # 队列
@@ -6,28 +7,29 @@ from queue import Queue
 # 多线程包
 from threading import Thread
 
-def imgdow(url,path):
+
+def imgdow(url, path):
     # 创建请求头
     headers = {
-        'User-Agent': 
+        'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
     }
 
     # 对网站进行请求
-    response = requests.get(url,headers = headers)
+    response = requests.get(url, headers=headers)
     # print(response.text,type(response.text))  #<class 'str'>
 
     # 解析响应
-    soup = BeautifulSoup(response.text,'lxml')
+    soup = BeautifulSoup(response.text, 'lxml')
     # print(soup,type(soup))  #<class 'bs4.BeautifulSoup'>
-    imglist = soup.find_all('img',attrs = {'class':'ui image lazy'})
+    imglist = soup.find_all('img', attrs={'class': 'ui image lazy'})
     # print(imglist,type(imglist))  #[.,.,.] <class 'bs4.element.ResultSet'>
 
     # 遍历数据,提取价值数据
     for img in imglist:
         imgName = img.get('title')
         imgUrl = img.get('data-original')
-        print(imgName,imgUrl)
+        print(imgName, imgUrl)
 
         # 保存数据
         try:
@@ -35,40 +37,43 @@ def imgdow(url,path):
             # response.text 返回的是一个unicode型的文本数据
             # response.content 返回的是bytes型的二进制数据
 
-            with open(path + imgName + os.path.splitext(imgUrl)[-1],'wb') as f:
-                img = requests.get(imgUrl,headers = headers).content
+            with open(path + imgName + os.path.splitext(imgUrl)[-1], 'wb') as f:
+                img = requests.get(imgUrl, headers=headers).content
                 f.write(img)
                 print(f'下载成功,{imgName}')
         except Exception as msg:
             print(f'下载失败,{msg}')
 
 # 创建多线程类
+
+
 class DownloadImg(Thread):
     # 重新__init__方法
-    def __init__(self,queue,path):
+    def __init__(self, queue, path):
         Thread.__init__(self)
         self.queue = queue
         self.path = path
 
         if not os.path.exists(path):
             os.mkdir(path)
-    
+
     # 重写run方法
     def run(self):
         while True:
             # queue.get([block[,timeout]]) 获取队列,timeout等待时间
             url = self.queue.get()
             try:
-                imgdow(url,self.path)
+                imgdow(url, self.path)
             except Exception as msg:
                 print(f'下载失败,{msg}')
             finally:
                 # 线程在队列中取完元素之后后者退出之后发送一个结束限号
                 self.queue.task_done()
 
+
 if __name__ == '__main__':
     urlName = 'https://fabiaoqing.com/biaoqing/lists/page/{pageNum}.html'
-    urls = [urlName.format(pageNum = pageNum) for pageNum in range(1,2)]
+    urls = [urlName.format(pageNum=pageNum) for pageNum in range(1, 2)]
     # print(urls)
 
     queue = Queue()
@@ -76,7 +81,7 @@ if __name__ == '__main__':
 
     # 创建线程
     for x in range(3):
-        rookie = DownloadImg(queue,path)
+        rookie = DownloadImg(queue, path)
 
         # 创建守护线程
         # 如果子线程为守护线程的话,主线程结束时不会监测子线程是否结束任务直接退出
--- Read-Search-Ask/Python/爬虫/爬取视频/videoSpider.py
+++ Read-Search-Ask/Python/爬虫/爬取视频/videoSpider.py
@@ -10,19 +10,25 @@
 #     在不需要刷新整个野蛮的情况下,对页面进行局部刷新
 
 import requests
-import re,os,pprint
+import re
+import os
+import pprint
+
 
 def videoSpider(url):
     # 请求头中用户凭证
     headers = {
-        'User-Agent': 
+        'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
     }
 
     # 获取数据
-    response = requests.get(url,headers)
+    response = requests.get(url, headers)
     jsonData = response.json()
     pprint.pprint(jsonData)
+
+    # 创建文件夹
+    os.makedirs('video/')
 
     # 解析数据(视频地址)
     datalist = jsonData['content']['list']
@@ -32,19 +38,20 @@ def videoSpider(url):
         videoUrl = data['playurl']
         # 去除标题中的非法字符
         pattern = re.compile("[^\u4e00-\u9fa5^.^a-z^A-Z^0-9]")
-        newTitle = re.sub(pattern,'',videoTitle)
+        newTitle = re.sub(pattern, '', videoTitle)
 
         # 请求视频数据
-        videoData = requests.get(url=videoUrl,headers=headers).content
+        videoData = requests.get(url=videoUrl, headers=headers).content
 
         # 数据保存
-        with open('video/' + newTitle + os.path.splitext(videoUrl)[-1],'wb+') as f:
+        with open('video/' + newTitle + os.path.splitext(videoUrl)[-1], 'wb+') as f:
             f.write(videoData)
-            print('保存完成'+ newTitle)
+            print('保存完成' + newTitle)
+
 
 if __name__ == '__main__':
     # 获取的数据源
     urlOri = 'http://www.6.cn/minivideo/getMiniVideoList.php?act=recommend&page={page}&pagesize=20'
-    urls = [urlOri.format(page=page) for page in range(1,11)]
+    urls = [urlOri.format(page=page) for page in range(1, 2)]
     for url in urls:
         videoSpider(url)
