4fbdf240c02a9206e9750dd79dc3dbd4bd629fd1
wxchan <595366700@qq.com>
2021-03-10 16:36:48 +0800

    keep coding

--- Read-Search-Ask/Python/爬虫/爬取电商数据/jdSpider.py
+++ Read-Search-Ask/Python/爬虫/爬取电商数据/jdSpider.py
@@ -2,14 +2,11 @@ import csv
 import time
 from selenium import webdriver
 
-# 搜索商品
-def getProduct(key):
-    pass
 
 # 控制下拉页面
 def dropDown():
     # 控制下拉次数 5次
-    for x in range(1,11,2):
+    for x in range(1, 11, 2):
         time.sleep(0.5)
         j = x/10  # 取十分位,页面长度的百分比
         js = f'document.documentElement.scrollTop = document.documentElement.scrollHeight * {j}'
@@ -23,56 +20,50 @@ def parseData():
     for ware in wares:
         try:
             # 商品名称
-            name = ware.find_element_by_css_selector('.gl-item div.p-name a em').text
-            name = name.replace('\n','')
+            name = ware.find_element_by_css_selector(
+                '.gl-item div.p-name a em').text
+            name = name.replace('\n', '')
             # 商品价格
-            price = ware.find_element_by_css_selector('.gl-item div.p-price strong i').text
+            price = ware.find_element_by_css_selector(
+                '.gl-item div.p-price strong i').text
             # 商品评论
-            comment = ware.find_element_by_css_selector('.gl-item div.p-commit strong a').text
+            comment = ware.find_element_by_css_selector(
+                '.gl-item div.p-commit strong a').text
             # 商品店铺
-            mallName = ware.find_element_by_css_selector('.gl-item div.p-shop span a').text
-            print(name,price,comment,mallName)
+            mallName = ware.find_element_by_css_selector(
+                '.gl-item div.p-shop span a').text
+            print(name, price, comment, mallName)
         except:
             continue
 
         # 数据保存
-        with open('京东数据.csv',mode='a',encoding='utf-8',newline='') as f:
+        with open('京东数据.csv', mode='a', encoding='utf-8', newline='') as f:
             csvWrite = csv.writer(f)
-            csvWrite.writerow([name,price,comment,mallName])
+            csvWrite.writerow([name, price, comment, mallName])
 
 # 点下一页
-def getNext():
-    webCrome.find_element_by_css_selector('.p-wrap > span.p-num > a.pn-next > em').click()
+def nextPage():
+    webCrome.find_element_by_css_selector(
+        '.p-wrap > span.p-num > a.pn-next > em').click()
 
-
-# 创建浏览驱动对象
+# 创建驱动对象
 webCrome = webdriver.Chrome()
-
 # 使用驱动对象请求网页
 webCrome.get('https://www.jd.com/')
-
-# 需要查询的关键字
-keyword = '手机'
-
-# 搜索框中输入keyword
-webCrome.find_element_by_css_selector('#key').send_keys(keyword)
-
+# 搜索框中输入self.name
+webCrome.find_element_by_css_selector('#key').send_keys('手机')
 # 点击搜索按钮
-webCrome.find_element_by_css_selector('#search > div > div.form > button').click()
-
+webCrome.find_element_by_css_selector(
+    '#search > div > div.form > button').click()
 # 隐式等待页面加载完成
 webCrome.implicitly_wait(10)
-
 # 最大化浏览器
 webCrome.maximize_window()
 
-for page in range(1,10):
+for i in range(1, 10):
     # 下拉
     dropDown()
     # 解析
     parseData()
     # 点下一页
-    getNext()
-
-# 关闭驱动对象
-# webCrome.quit()
+    nextPage()
