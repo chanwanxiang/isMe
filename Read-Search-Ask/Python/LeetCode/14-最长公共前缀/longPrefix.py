9c8988a48008777b751924721420d39c69339c3f
wxchan <wxchannive@gmail.com>
2021-03-20 23:57:38 +0800

    keep coding

--- Read-Search-Ask/Python/LeetCode/14-最长公共前缀/longPrefix.py
+++ Read-Search-Ask/Python/LeetCode/14-最长公共前缀/longPrefix.py
@@ -7,25 +7,38 @@
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 # 方式一
+# class Solution(object):
+#     def longPrefix(self,strs):
+#         result = ''
+#         i = 0
+        
+#         while True:
+#             try:
+#                 sets = set(string[i] for string in strs)
+#                 if len(sets) == 1:
+#                     result += sets.pop()
+#                     i += 1
+#                 else:
+#                     break
+#             except Exception:
+#                 break
+
+#         return result
+
+# 方式二
 class Solution(object):
     def longPrefix(self,strs):
         result = ''
-        i = 0
-        
-        while True:
-            try:
-                sets = set(string[i] for string in strs)
-                if len(sets) == 1:
-                    result += sets.pop()
-                    i += 1
-                else:
+
+        for i in range(len(strs[0])):
+            for string in strs[1:]:
+                if len(string) <= i  or strs[0][i] != string[i]:
                     break
-            except Exception:
-                break
+            else:
+                result += strs[0][i]
 
         return result
 
-# 方式二
 
 s = Solution()
 print(s.longPrefix(['flower','flow','flo']))
