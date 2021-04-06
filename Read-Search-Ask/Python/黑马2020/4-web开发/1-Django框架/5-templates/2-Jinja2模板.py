e12414f621aea50e25b37d95adc77be10632a80a
wxchan <595366700@qq.com>
2021-04-06 15:20:42 +0800

    keep coding

--- Read-Search-Ask/Python/黑马2020/4-web开发/1-Django框架/5-templates/2-Jinja2模板.py
+++ Read-Search-Ask/Python/黑马2020/4-web开发/1-Django框架/5-templates/2-Jinja2模板.py
@@ -1,3 +1,6 @@
 # django使用jinja2模板
-    jinja2介绍
-    
+#     1)安装jinja2模块
+#         pip install jinja2
+#     2)django配置jinja2
+#         项目文件中创建jinja2_env.py文件
+#         在setting中TEMPLATES配置janja2环境
