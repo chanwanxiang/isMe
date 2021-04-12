e80fc19aa16d3cf1e906fa0b9f8760084a39598b
wxchan <595366700@qq.com>
2021-04-12 15:33:52 +0800

    keep coding

--- Read-Search-Ask/Python/LeetCode/9-回文数/isPalindrome.py
+++ Read-Search-Ask/Python/LeetCode/9-回文数/isPalindrome.py
@@ -1,10 +1,9 @@
-# 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+# 给你一个整数x,如果x是一个回文整数,返回true;否则,返回false
 
-# 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+# 回文数是指正序(从左向右)和倒序(从右向左)读都是一样的整数,例如,121是回文,而123不是
 
-# 来源：力扣（LeetCode）
-# 链接：https://leetcode-cn.com/problems/palindrome-number
-# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+# 来源:力扣(LeetCode)
+# 链接:https://leetcode-cn.com/problems/palindrome-number
 
 class Solution(object):
     def isPal(self, x):
