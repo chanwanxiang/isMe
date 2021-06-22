bbb0164e2329b2f1aa302bd46d8b22d7110fa069
wxchan <595366700@qq.com>
2021-06-22 17:22:48 +0800

    keep coding

--- Read-Search-Ask/Python/测试/小滴课堂/Python接口自动化/apiTest/case/indexTestCase.py
+++ Read-Search-Ask/Python/测试/小滴课堂/Python接口自动化/apiTest/case/indexTestCase.py
@@ -1,12 +1,13 @@
-# -*- coding:UTF-8 -*-
+import os
+import sys
 import unittest
-import os,sys
-sys.path.append('../apiTest')
-
 from util.requestUtil import RequestUtil
+
+sys.path.append('../apiTest')
 
 # 主机地址
 host = 'https://api.xdclass.net'
+
 
 # 首页测试用例案例
 class indexTestCase(unittest.TestCase):
@@ -16,35 +17,37 @@ class indexTestCase(unittest.TestCase):
         # 创建request对象
         req = RequestUtil()
 
-        #首页模块 分类列表
+        # 首页模块 分类列表
         url = host + '/pub/api/v1/web/all_category'
-        response = req.request(url,'get')
+        response = req.request(url, 'get')
 
         # 判断响应的状态码是否正常 断言
         self.assertEqual(response['code'], 0, '业务状态码不正常')
 
         # 判断分类列表长度 断言
-        self.assertTrue(len(response['data'])>0, '首页分类列表为空')
+        self.assertTrue(len(response['data']) > 0, '首页分类列表为空')
         pass
-    
-    #首页视频列表 
+
+    # 首页视频列表
     def testindexVideolist(self):
         req = RequestUtil()
 
-        #首页模块 分类列表
+        # 首页模块 分类列表
         url = host + '/pub/api/v1/web/index_card'
-        response = req.request(url,'get')
+        response = req.request(url, 'get')
 
         # 判断响应的状态码是否正常
         self.assertEqual(response['code'], 0, '业务状态码不正常')
 
         # 判断视频卡片列表长度
-        self.assertTrue(len(response['data'])>0, '视频卡片列表为空')
+        self.assertTrue(len(response['data']) > 0, '视频卡片列表为空')
         videoCardlist = response['data']
 
         # 判断视频卡片列表长度并返回错误值
         for card in videoCardlist:
-            self.assertTrue(len(card['title'])>0, '视频卡片标题为空,id=' + str(card['id']))
-    
+            self.assertTrue(len(card['title']) > 0,
+                            '视频卡片标题为空,id=' + str(card['id']))
+
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
