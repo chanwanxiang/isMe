32760a1e91a8ebf96ac40b5c643f128c93e6c4d3
wxchan <595366700@qq.com>
2021-06-25 14:55:51 +0800

    keep coding

--- Read-Search-Ask/Python/测试/小滴课堂/Python接口自动化/apiTest/util/requestUtil.py
+++ Read-Search-Ask/Python/测试/小滴课堂/Python接口自动化/apiTest/util/requestUtil.py
@@ -1,6 +1,6 @@
 import pprint
 import requests
-
+# from util import 
 
 # HTTP工具类封装 每个请求需要做异常捕获,日志记录,协议转换,封装工具方便进行统一维护
 class RequestUtil:
@@ -35,9 +35,11 @@ if __name__ == "__main__":
     # result = r.request(url,'get')
     # pprint.pprint(result)
 
-    url = 'https://api.xdclass.net/pub/api/v1/web/web_login'
+    # url = 'https://api.xdclass.net/pub/api/v1/web/web_login'
+    url = 'https://signtest.yuuwei.com/bank/fv/detail/2021052517140012'
     r = RequestUtil()
-    # headers = {'Content-Type':'application/x-www-form-urlencoded'}
-    data = {'phone': '13113777555', 'pwd': '1234567890'}
-    result = r.request(url, 'post', params=data)
+    headers = {'Content-Type':'application/x-www-form-urlencoded',
+        'token':'cefa30b1-a287-4ee9-ba4d-047384812a7e'}
+    # data = {'phone': '13113777555', 'pwd': '1234567890'}
+    result = r.request(url, 'get', headers=headers)
     pprint.pprint(result)
